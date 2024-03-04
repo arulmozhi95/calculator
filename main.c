8dfe1908b96b38f0f48375935c9d4ababab66878
arulmozhi95 <arulboobalan95@gmail.com>
2024-03-04 18:04:23 +0530

    almost complete

--- main.c
+++ main.c
@@ -2,8 +2,27 @@
 #include<stdlib.h>
 #include<math.h>
 int main(){
-    int val=1;
-    while(val==1){
-        
+    char a;
+    scanf("%c",&a);
+    int num1,num2;
+    scanf("%d,%d",&num1,&num2);
+    switch (a){
+        case '+':
+           return num1+num2;
+           break;
+        case '-':
+            return num1-num2;
+            break;
+        case '*':
+            return num1*num2;
+            break;
+        case '%':
+            return num1%num2;
+            break;
+        case '/':
+            return num1/num2;
+            break;
     }
+    return 0;
+       
 }
